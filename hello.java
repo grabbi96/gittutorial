62f8810656ef8d15b3b4bb95321745fc42b2bce8
grabbi <grabbi96@gmail.com>
2018-07-04 21:36:24 -0700

    new method added

--- hello.java
+++ hello.java
@@ -2,4 +2,8 @@ public class hello{
     public static void main (String[] args){
         System.out.printIn('this is  ');
     }
+
+    public static void main (String[] args){
+        System.out.printIn('this is  ');
+    }
 }
